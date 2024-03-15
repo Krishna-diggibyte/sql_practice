bbf34d8196f063f76d05245473e57eb8922cc2b5
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-15 17:13:02 +0530

    create a employee table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,10 @@
-use sql_practice
-create table employee
+create database empdb;
+use empdb
+
+create table emp_detail(
+						emp_id int ,
+						emp_name varchar(15),
+						emp_email varchar(30),
+						emp_gender char (1),
+						emp_loc varchar(20),
+						PRIMARY KEY (emp_id));
